79d51775c816d80bb03bac472eab23bf3ec341ff
unixlinuxgeek <unixlinuxgeek@gmail.com>
2023-09-07 22:07:40 +0500

    Added ch2/tempconv

--- ch2/tempconv/tempconv.go
+++ ch2/tempconv/tempconv.go
@@ -1,22 +1,14 @@
-// Добавьте в пакет tempconv типы, константы и функции
-// для работы с температурой по шкале Кельвина,
-// в которой нуль градусов соответствует температуре -273,15 °C
-// а разница температур в 1K имеет ту же величину,
-// что и 1 °С
-
 package tempconv
 
 import "fmt"
 
 type Cel float64
 type Far float64
-type Kel float64
 
 const (
 	AbsoluteZeroC Cel = -273.15
 	FreezingC     Cel = 0
 	BoilingC      Cel = 100
-	KelvinZeroC   Cel = -273.15
 )
 
 func (c Cel) String() string {
@@ -25,8 +17,4 @@ func (c Cel) String() string {
 
 func (f Far) String() string {
 	return fmt.Sprintf("%g ℉", f)
-}
-
-func (k Kel) String() string {
-	return fmt.Sprintf("%g °K", k)
 }
