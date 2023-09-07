d863d0fb336ac78bf84c13b0466a158729220aca
unixlinuxgeek <unixlinuxgeek@gmail.com>
2023-09-07 18:28:13 +0500

    Created cf

---
+++ ch2/cf/cf.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"github.com/unixlinuxgeek/The_Go_Programming_Language_Exercises/ch2/tempconv"
+	"os"
+	"strconv"
+)
+
+func main() {
+	for _, arg := range os.Args[1:] {
+		t, err := strconv.ParseFloat(arg, 64)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "cf: %v\n", err)
+			os.Exit(1)
+		}
+		f := tempconv.Far(t)
+		c := tempconv.Cel(t)
+		fmt.Printf("%s = %s, %s = %s\n", f, tempconv.FToC(f), c, tempconv.CToF(c))
+	}
+}
--- ch2/conv/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/unixlinuxgeek/The_Go_Programming_Language_Exercises/ch2/conv
-
-go 1.20
