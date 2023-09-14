13bdc927bbb5db58c146bd6b4a95aad2780dff63
unixlinuxgeek <unixlinuxgeek@gmail.com>
2023-09-14 16:09:03 +0500

    Updated ch2/popcount/popcount

--- ch2/popcount/popcount.go
+++ ch2/popcount/popcount.go
@@ -12,5 +12,13 @@ func init() {
 // PopCount возвращает степень заполнения
 // (количество установленных битов) значения x.
 func PopCount(x uint64) int {
-	return int(pc[byte(x>>(0*8))] + pc[byte(x>>1*8)] + pc[byte(x>>2*8)] + pc[byte(x>>3*8)] + +pc[byte(x>>4*8)] + pc[byte(x>>5*8)] + pc[byte(x>>6*8)] + pc[byte(x>>7*8)])
+	return int(
+		pc[byte(x>>(1*8))] +
+			pc[byte(x>>(2*8))] +
+			pc[byte(x>>(3*8))] +
+			pc[byte(x>>(4*8))] +
+			pc[byte(x>>(5*8))] +
+			pc[byte(x>>(6*8))] +
+			pc[byte(x>>(7*8))])
+
 }
