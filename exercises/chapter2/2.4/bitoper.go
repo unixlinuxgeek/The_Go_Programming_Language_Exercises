aa232cfd456e4b6c411861f4deba1b7320a0e9ca
unixlinuxgeek <unixlinuxgeek@gmail.com>
2023-09-19 22:34:56 +0500

    Fix

--- exercises/chapter2/2.4/bitoper.go
+++ exercises/chapter2/2.4/bitoper.go
@@ -1,11 +1,11 @@
 package popcount
 
 // Побитовый сдвиг влево
-func BitShiftingLeft(x uint64, y uint) uint64 {
+func BitShiftLeft(x uint64, y uint) uint64 {
 	return x << y
 }
 
 // Побитовый сдвиг вправо
-func BitShiftingRight(x uint64, y uint64) uint64 {
+func BitShiftRight(x uint64, y uint64) uint64 {
 	return x >> y
 }
