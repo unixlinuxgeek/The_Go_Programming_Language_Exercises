26e15f3a70845754dec158f789fd3e701f7cfb0f
unixlinuxgeek <unixlinuxgeek@gmail.com>
2023-09-21 17:14:20 +0500

    Added chapter 4 folders

--- exercises/chapter2/2.5/popcount.go
+++ exercises/chapter2/2.5/popcount.go
@@ -27,3 +27,25 @@ func PopCount(x uint64) int {
 			pc[byte(x>>(7*8))])
 
 }
+
+func v(x uint64) int {
+	// x&(x-1)
+	xX := x & (x - 1)
+	return int(
+		pc[byte(xX>>(1*8))] +
+			pc[byte(xX>>(2*8))] +
+			pc[byte(xX>>(3*8))] +
+			pc[byte(xX>>(4*8))] +
+			pc[byte(xX>>(5*8))] +
+			pc[byte(xX>>(6*8))] +
+			pc[byte(xX>>(7*8))])
+}
+
+func PopCount3(x int) int {
+	cnt := 0
+	for x != 0 {
+		cnt = x & 1
+		x >>= 1
+	}
+	return cnt
+}
