a3eaa88e8a30f44249b2fc584185a66e866e7823
unixlinuxgeek <unixlinuxgeek@gmail.com>
2023-11-05 17:29:16 +0500

    Added ch7/sorting.go

--- ch7/sorting.go
+++ ch7/sorting.go
@@ -18,7 +18,7 @@ type Track struct {
 var tracks = []*Track{
 	{"Go", "Delilah", "From the Roots Up", 2012, length("3m38s")},
 	{"Go", "Moby", "Moby", 1992, length("3m37s")},
-	{"", "", "", 2007, length("4m36s")},
+	{"Go Ahead", "Alicia Keys", "As I Am", 2007, length("4m36s")},
 	{"Ready 2 Go", "Martin Solveig", "Smash", 2011, length("4m24s")},
 }
 
