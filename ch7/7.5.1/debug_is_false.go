3c63549e90de4b5194ae0c2267ce136c2dd5e436
unixlinuxgeek <unixlinuxgeek@gmail.com>
2023-11-05 16:35:09 +0500

    Added debug is false example

---
+++ ch7/7.5.1/debug_is_false.go
@@ -0,0 +1,43 @@
+// debug is false
+//  $ go run ./debug_is_false.go
+//  panic: runtime error: invalid memory address or nil pointer dereference
+//  [signal SIGSEGV: segmentation violation code=0x1 addr=0x20 pc=0x462e1d]
+//
+//  goroutine 1 [running]:
+//  bytes.(*Buffer).Write(0x404b99?, {0xc00007cf5c?, 0x0?, 0x0?})
+//        /usr/local/go/src/bytes/buffer.go:168 +0x1d
+//  main.f(...)
+//        /home/geek/GoApps/The_Go_Programming_Language_Exercises/ch7/7.5.1/debug_is_false.go:26
+//  main.main()
+//        /home/geek/GoApps/The_Go_Programming_Language_Exercises/ch7/7.5.1/debug_is_false.go:16 +0x55
+//  exit status 2
+//
+
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+)
+
+const debug = false
+
+func main() {
+	var buf *bytes.Buffer
+	if debug {
+		buf = new(bytes.Buffer) //  Накопление вывода
+	}
+	f(buf)
+	if debug {
+		// используем buf...
+		fmt.Println("Debug turn on...")
+	}
+}
+
+func f(out io.Writer) {
+	// ... некоторые действия ...
+	if out != nil {
+		out.Write([]byte("выполнено:\n"))
+	}
+}
